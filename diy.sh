2887f5b5bf767837b4e8a28e72966853f0fec3fe
yuhuan1981 <35647879+yuhuan1981@users.noreply.github.com>
2020-11-26 21:01:10 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,3 +7,6 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# Add luci-app-ssr-plus
+pushd package/lean
+git clone --depth=1 https://github.com/fw876/helloworld
